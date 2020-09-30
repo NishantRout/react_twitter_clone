c5331de37577db746878294869dd67fc5b2be40a
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-30 22:54:25 +0530

    twitter feed

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './css/TweetBox.css';
+import { Avatar, Button } from '@material-ui/core';
 
 function TweetBox() {
     return (
-        <div className="">
-
+        <div className="tweetBox">
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar
+                        src="https://pbs.twimg.com/profile_images/1243512060564189185/SMFgwobl.png"
+                    />
+                    <input
+                        placeholder="What's happening?"
+                        type="text"
+                    />
+                </div>
+                <input
+                    className="tweetBox__imageInput"
+                    placeholder="Optional: Enter image URL"
+                    type="text"
+                />
+                <Button className="tweetBox__tweetButton">Tweet</Button>
+            </form>
         </div>
     )
 }
