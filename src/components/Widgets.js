3773e7321501628301425960bcde8a2a968fa50a
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-10-01 01:24:41 +0530

    widgets

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './css/Widgets.css';
+import { Search } from '@material-ui/icons';
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h2>Widgets</h2>
+            <div className="widgets__input">
+                <Search className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+                <TwitterTweetEmbed tweetId={"1304460762144845824"} />
+
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="perfect_shades"
+                    options={{ height: 400 }}
+                />
+
+                <TwitterShareButton
+                    url={"https://www.facebook.com/perfectshades.99"}
+                    options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+                />
+            </div>
         </div>
     )
 }
