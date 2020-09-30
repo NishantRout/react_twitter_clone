e1e8f47c099c59e5814cf3c50c0a1a7345584814
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-30 17:11:44 +0530

    sidebar ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Twitter Clone</h1>
+      <Sidebar />
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,30 @@
+import { Button } from '@material-ui/core';
+import { BookmarkBorder, Home, ListAlt, MailOutline, MoreHoriz, NotificationsNone, PermIdentity, Search, Twitter } from '@material-ui/icons';
 import React from 'react';
+import './css/Sidebar.css';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
     return (
         <div className="sidebar">
+            <Twitter />
 
+            <SidebarOption active Icon={Home} text="Home" />
+            <SidebarOption Icon={Search} text="Explore" />
+            <SidebarOption Icon={NotificationsNone} text="Notifications" />
+            <SidebarOption Icon={MailOutline} text="Messages" />
+            <SidebarOption Icon={BookmarkBorder} text="Bookmarks" />
+            <SidebarOption Icon={ListAlt} text="Lists" />
+            <SidebarOption Icon={PermIdentity} text="Profile" />
+            <SidebarOption Icon={MoreHoriz} text="More" />
+
+            <Button
+                variant="outlined"
+                className="sidebar__tweet"
+                fullWidth
+            >
+                Tweet
+            </Button>
         </div>
     )
 }
