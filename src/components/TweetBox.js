af36800e57d5ef4f061af7f2b2f814f4dee2a1fe
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-10-02 01:16:16 +0530

    send a tweet functionality

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import './css/TweetBox.css';
-import { Avatar, Button } from '@material-ui/core';
+import React, { useState } from "react";
+import "./css/TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+import db from "../config/firebase";
 
 function TweetBox() {
-    return (
-        <div className="tweetBox">
-            <form>
-                <div className="tweetBox__input">
-                    <Avatar
-                        src="https://pbs.twimg.com/profile_images/1243512060564189185/SMFgwobl.png"
-                    />
-                    <input
-                        placeholder="What's happening?"
-                        type="text"
-                    />
-                </div>
-                <input
-                    className="tweetBox__imageInput"
-                    placeholder="Optional: Enter image URL"
-                    type="text"
-                />
-                <Button className="tweetBox__tweetButton">Tweet</Button>
-            </form>
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      displayName: "Perfect Shades",
+      username: "perfectshades.99",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://pbs.twimg.com/profile_images/1243512060564189185/SMFgwobl.png",
+    });
+    setTweetImage("");
+    setTweetMessage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://pbs.twimg.com/profile_images/1243512060564189185/SMFgwobl.png" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
         </div>
-    )
+        <input
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+        <Button onClick={sendTweet} className="tweetBox__tweetButton">
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default TweetBox;
