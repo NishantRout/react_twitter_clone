e32c68f03966f39edb18651d7c361c5ab4103b12
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-10-02 01:40:21 +0530

    react-flip-move & firebase deploy

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,6 +3,7 @@ import "./css/Feed.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
 import db from "../config/firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -21,19 +22,19 @@ function Feed() {
 
       <TweetBox />
 
-      {posts.map((post) => (
-        <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          image={post.image}
-          avatar={post.avatar}
-        />
-      ))}
-      {/* <Post />
-            <Post />
-            <Post /> */}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            image={post.image}
+            avatar={post.avatar}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
